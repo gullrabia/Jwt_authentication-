e1970968dab49aecdf020aa603f341f6478da0eb
Rabia gull <gullrabia666@gmail.com>
2026-05-23 18:07:16 +0500

    vercel change

--- server/server.js
+++ server/server.js
@@ -1,31 +1,37 @@
 import express from "express";
-import 'dotenv/config.js'
-import cors from 'cors'
+import "dotenv/config";
+import cors from "cors";
 import cookieParser from "cookie-parser";
+
 import connectDB from "./config/mongodb.js";
-import authRouter from './routes/authRoutes.js'
+import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
-
-
 
 const app = express();
 
-const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
-
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://your-frontend.vercel.app"
+];
 
 app.use(express.json());
-app.use(cors({origin: allowedOrigins, credentials: true}));
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 
-//Api endpoint
-app.get('/', (req, res)=> res.send('Api is working....'))
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API is working...");
+});
 
-app.use('/api/auth', authRouter);
-app.use('/api/user', userRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-
-app.listen(port, ()=> console.log(`Server Started on port: ${port}`));
-
+export default app;
